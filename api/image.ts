a28f10bbebf2528c56d52da50de4cb0e60585924
JayJay1024 <lygm1062@gmail.com>
2024-02-02 03:42:45 +0800

    test

--- api/image.ts
+++ api/image.ts
@@ -35,77 +35,6 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
       }),
     };
 
-    // const svg = await satori(
-    //   {
-    //     type: "div",
-    //     key: "1",
-    //     props: {
-    //       style: {
-    //         justifyContent: "flex-start",
-    //         alignItems: "center",
-    //         display: "flex",
-    //         width: "100%",
-    //         height: "100%",
-    //         backgroundColor: "#f2f3f5",
-    //         padding: 50,
-    //         lineHeight: 1.2,
-    //         fontSize: 24,
-    //       },
-    //       children: {
-    //         type: "div",
-    //         key: "11",
-    //         props: {
-    //           style: {
-    //             display: "flex",
-    //             flexDirection: "column",
-    //             padding: 20,
-    //           },
-    //           children: [
-    //             {
-    //               type: "div",
-    //               key: "111",
-    //               props: {
-    //                 style: {
-    //                   color: "#00f",
-    //                 },
-    //                 children: pollData.options.map((opt, index) => ({
-    //                   type: "div",
-    //                   key: `11${index}`,
-    //                   props: {
-    //                     style: {
-    //                       backgroundColor: voted ? "#ff0083" : "",
-    //                       color: "#f2f3f5",
-    //                       padding: 10,
-    //                       marginBottom: 10,
-    //                       borderRadius: 4,
-    //                       width: `${voted ? opt.percentOfTotal : 100}%`,
-    //                       whiteSpace: "nowrap",
-    //                       overflow: "visible",
-    //                     },
-    //                     children: opt.text,
-    //                   },
-    //                 })),
-    //               },
-    //             },
-    //           ],
-    //         },
-    //       },
-    //     },
-    //   },
-    //   {
-    //     width: 600,
-    //     height: 400,
-    //     fonts: [
-    //       {
-    //         data: fontData,
-    //         name: "Roboto",
-    //         style: "normal",
-    //         weight: 400,
-    //       },
-    //     ],
-    //   }
-    // );
-
     const svg = await satori(
       {
         type: "div",
@@ -124,30 +53,37 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
           },
           children: {
             type: "div",
-            key: "11",
+            key: "12",
             props: {
               style: {
                 display: "flex",
                 flexDirection: "column",
                 padding: 20,
               },
-              children: pollData.options.map((opt, index) => ({
-                type: "div",
-                key: `11${index}`,
-                props: {
-                  style: {
-                    backgroundColor: voted ? "#ff0083" : "",
-                    color: "#000",
-                    padding: 10,
-                    marginBottom: 10,
-                    borderRadius: 4,
-                    width: `${voted ? opt.percentOfTotal : 100}%`,
-                    whiteSpace: "nowrap",
-                    overflow: "visible",
-                  },
-                  children: opt.text,
+              children: [
+                {
+                  type: "h2",
+                  key: "123",
+                  props: { style: { textAlign: "center", color: "#ff0083" }, children: poll.title },
                 },
-              })),
+                ...pollData.options.map((opt, index) => ({
+                  type: "div",
+                  key: `1234${index}`,
+                  props: {
+                    style: {
+                      backgroundColor: voted ? "#ff0083" : "",
+                      color: "#000",
+                      padding: 10,
+                      marginBottom: 10,
+                      borderRadius: 4,
+                      width: `${voted ? opt.percentOfTotal : 100}%`,
+                      whiteSpace: "nowrap",
+                      overflow: "visible",
+                    },
+                    children: opt.text,
+                  },
+                })),
+              ],
             },
           },
         },
