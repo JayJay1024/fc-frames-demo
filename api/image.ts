4acf9da70ad82170d3e64a7eaf6b42035cd96025
JayJay1024 <lygm1062@gmail.com>
2024-02-02 03:22:58 +0800

    test

--- api/image.ts
+++ api/image.ts
@@ -34,7 +34,6 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
         return { option, votes, text, percentOfTotal };
       }),
     };
-    return res.send(pollData.options.length);
 
     const svg = await satori(
       {
