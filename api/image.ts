972ec0c495762a3697ea8b857d1d714c64619c41
JayJay1024 <lygm1062@gmail.com>
2024-02-02 03:00:31 +0800

    test

--- api/image.ts
+++ api/image.ts
@@ -68,7 +68,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
                     style: {
                       color: "#00f",
                     },
-                    children: "One",
+                    children: "One:" + pollData.options,length,
                   },
                 },
               ],
