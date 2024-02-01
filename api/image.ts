39155f186bfb872e0f3002dc77e5117fb12f3b39
JayJay1024 <lygm1062@gmail.com>
2024-02-01 22:53:32 +0800

    chore: test

--- api/image.ts
+++ api/image.ts
@@ -1,7 +1,15 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const pollId = req.query["id"];
-  res.statusCode = 200;
-  res.end(pollId ?? "Null");
+  try {
+    const pollId = req.query["id"];
+    if (!pollId) {
+      return res.status(400).send("Missing poll ID");
+    }
+
+    return res.status(200).send("Debug");
+  } catch (err) {
+    res.statusCode = 500;
+    return res.status(500).end("Error generating image");
+  }
 }
