33a39c22127fbf172f2d77851f9ad5361f476d67
JayJay1024 <lygm1062@gmail.com>
2024-02-02 02:50:24 +0800

    test

--- api/image.ts
+++ api/image.ts
@@ -47,38 +47,12 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
             width: "100%",
             height: "100%",
             backgroundColor: "#f2f3f5",
+            color: "#00f",
             padding: 50,
             lineHeight: 1.2,
             fontSize: 24,
           },
-          children: {
-            type: "div",
-            key: "11",
-            props: {
-              style: {
-                display: "flex",
-                flexDirection: "column",
-                padding: 20,
-              },
-              children: pollData.options.map((opt, index) => ({
-                type: "div",
-                key: `11${index}`,
-                props: {
-                  style: {
-                    backgroundColor: voted ? "#ff0083" : "",
-                    color: "#f2f3f5",
-                    padding: 10,
-                    marginBottom: 10,
-                    borderRadius: 4,
-                    width: `${voted ? opt.percentOfTotal : 100}%`,
-                    whiteSpace: "nowrap",
-                    overflow: "visible",
-                  },
-                  children: opt.text,
-                },
-              })),
-            },
-          },
+          children: "JayJay1024",
         },
       },
       {
@@ -96,47 +70,51 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     );
 
     // const svg = await satori(
-    //   <div
-    //     style={{
-    //       justifyContent: "flex-start",
-    //       alignItems: "center",
-    //       display: "flex",
-    //       width: "100%",
-    //       height: "100%",
-    //       backgroundColor: "#f2f3f5",
-    //       padding: 50,
-    //       lineHeight: 1.2,
-    //       fontSize: 24,
-    //     }}
-    //   >
-    //     <div
-    //       style={{
+    //   {
+    //     type: "div",
+    //     key: "1",
+    //     props: {
+    //       style: {
+    //         justifyContent: "flex-start",
+    //         alignItems: "center",
     //         display: "flex",
-    //         flexDirection: "column",
-    //         padding: 20,
-    //       }}
-    //     >
-    //       {pollData.options.map((opt, index) => {
-    //         return (
-    //           <div
-    //             key={index}
-    //             style={{
-    //               backgroundColor: voted ? "#ff0083" : "",
-    //               color: "#f2f3f5",
-    //               padding: 10,
-    //               marginBottom: 10,
-    //               borderRadius: 4,
-    //               width: `${voted ? opt.percentOfTotal : 100}%`,
-    //               whiteSpace: "nowrap",
-    //               overflow: "visible",
-    //             }}
-    //           >
-    //             {opt.text}
-    //           </div>
-    //         );
-    //       })}
-    //     </div>
-    //   </div>,
+    //         width: "100%",
+    //         height: "100%",
+    //         backgroundColor: "#f2f3f5",
+    //         padding: 50,
+    //         lineHeight: 1.2,
+    //         fontSize: 24,
+    //       },
+    //       children: {
+    //         type: "div",
+    //         key: "11",
+    //         props: {
+    //           style: {
+    //             display: "flex",
+    //             flexDirection: "column",
+    //             padding: 20,
+    //           },
+    //           children: pollData.options.map((opt, index) => ({
+    //             type: "div",
+    //             key: `11${index}`,
+    //             props: {
+    //               style: {
+    //                 backgroundColor: voted ? "#ff0083" : "",
+    //                 color: "#f2f3f5",
+    //                 padding: 10,
+    //                 marginBottom: 10,
+    //                 borderRadius: 4,
+    //                 width: `${voted ? opt.percentOfTotal : 100}%`,
+    //                 whiteSpace: "nowrap",
+    //                 overflow: "visible",
+    //               },
+    //               children: opt.text,
+    //             },
+    //           })),
+    //         },
+    //       },
+    //     },
+    //   },
     //   {
     //     width: 600,
     //     height: 400,
@@ -154,7 +132,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     const pngBuffer = await sharp(Buffer.from(svg)).toFormat("png").toBuffer();
     res.setHeader("Content-Type", "image/png");
     res.setHeader("Cache-Control", "max-age=10");
-    return res.send(pngBuffer);
+    return res.end(pngBuffer);
   } catch (err) {
     console.error(err);
     res.statusCode = 500;
